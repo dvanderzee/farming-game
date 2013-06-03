d27e59d6a8377f1770539672a306ad4f7feba292
David Vanderzee <dvanderzee4@gmail.com>
2013-06-02 20:39:32 -0400

    syncing csproj files.

--- Assets/CropTileClass.cs
+++ Assets/CropTileClass.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 using System.Collections;
 
 public class CropTileClass : MonoBehaviour {
-
+	
+	int Cropstage = 0; //0 is no crop. 1 is seed planted. 2 is watered. 3 is harvestable. 4 is tillable
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
@@ -13,8 +15,28 @@ public class CropTileClass : MonoBehaviour {
 	
 	}
 	
-	public void plant() {
-		Debug.Log("HELLO WORLD");	
-		this.transform.gameObject.GetComponent<Renderer>().material.color = new Color (Random.Range (0.0f,1.0f), Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f), Random.Range (0f,1f));
+	public void plantseed() {
+		//Debug.Log("HELLO WORLD");	
+		this.transform.gameObject.GetComponent<Renderer>().material.color = new Color (Color.green);//(Random.Range (0.0f,1.0f), Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f), Random.Range (0f,1f));
+		Cropstage = 1;
 	}
+	
+	public void watercrop () {
+		this.transform.gameObject.GetComponent<Renderer>().material.color = new Color (Color.blue);
+		Cropstage = 2;
+		
+		//You water multiple times until
+		//Cropstage = 3;
+		
+	}
+	
+	public void harvestcrop () {
+		this.transform.gameObject.GetComponent<Renderer>().material.color = new Color (Color.red);
+		Cropstage = 4;
+		
+	}
+	
+	public void tillcrop () {
+		this.transform.gameObject.GetComponent<Renderer>().material.color = new Color (153,128,0);
+		Cropstage = 0;
 }
--- Assets/ScriptsImade/Player.cs
+++ Assets/ScriptsImade/Player.cs
@@ -12,14 +12,14 @@ public class Player : MonoBehaviour {
 	void Update () {
 	
 	if( Input.GetMouseButtonDown(0)){
-			Debug.Log("Frame Updated");
+			//Debug.Log("Frame Updated");
 			RaycastHit hitinfo;
 			if(Physics.Raycast(this.transform.position, this.transform.forward, out hitinfo, this.Interactdistance, 1<<LayerMask.NameToLayer("Crops"))){
 				//Debug.Log(hitinfo.collider.gameObject);
-				Debug.Log("Hit sometrhing");
+				//Debug.Log("Hit sometrhing");
 				CropTileClass CropTileComponent = (CropTileClass)hitinfo.collider.gameObject.GetComponent("CropTileClass");
-				Debug.Log(CropTileComponent);
-				Debug.DrawLine(this.transform.position, hitinfo.point, Color.red, 0.5f);
+				//Debug.Log(CropTileComponent);
+				//Debug.DrawLine(this.transform.position, hitinfo.point, Color.red, 0.5f);
 				if(CropTileComponent != null){
 					CropTileComponent.plant();
 				}
